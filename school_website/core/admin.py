bbd8a18f1a54ac428810486e5cc42058960d8902
Sithysak Ngo <sngo1@angelo.edu>
2026-05-26 21:13:45 -0500

    Add files via upload

---
+++ school_website/core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(SiteSettings)
+admin.site.register(HeroSection)
+admin.site.register(AboutSection)
+admin.site.register(Facility)
+admin.site.register(Offer)
+admin.site.register(GalleryItem)
+admin.site.register(Application)
+admin.site.register(LocationSection)
+admin.site.register(ContactSection)
+admin.site.register(ContactMessage)
